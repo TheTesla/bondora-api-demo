f079911d30168c2f633d01d49e72c6da247db217
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2019-03-31 21:51:23 +0200

    added profit and parts of it, purchase price

--- main.py
+++ main.py
@@ -22,12 +22,24 @@ headers = { 'Authorization': 'Bearer {}'.format(bearer),
 r = requests.get('https://api.bondora.com/api/v1/account/investments', headers=headers)
 invests = r.json()
 
+startBonus = 5.00
+
 try:
+    issued = sum([e['PurchasePrice'] for i, e in enumerate(invests['Payload']) if e['LoanStatusCode'] not in [3]])
     outstandingPrincipal = sum([e['PrincipalRemaining'] for i, e in enumerate(invests['Payload']) if e['LoanStatusCode'] in [2,5,100]])
     overduePrincipal = sum([e['PrincipalLateAmount'] for i, e in enumerate(invests['Payload'])])
+    earnings = sum([e['NoteLoanTransfersEarningsAmount'] for i, e in enumerate(invests['Payload'])])
+    print("Loans Issued (price)     {: 10.2f}".format(issued))
+    print("                                  ")
     print("Account Value:           {: 10.2f}".format(outstandingPrincipal-overduePrincipal))
     print("  Outstanding Principal: {: 10.2f}".format(outstandingPrincipal))
     print("  Overdue Principal:     {: 10.2f}".format(-overduePrincipal))
+    print("                                  ")
+    print("Profit:                  {: 10.2f}".format(earnings+startBonus-overduePrincipal))
+    print("  Start Bonus:           {: 10.2f}".format(startBonus))
+    print("  Gross Profit:          {: 10.2f}".format(earnings-overduePrincipal))
+    print("    Interest Paid        {: 10.2f}".format(earnings))
+    print("    Overdue Principal:   {: 10.2f}".format(-overduePrincipal))
 except:
     print(invests)
 
